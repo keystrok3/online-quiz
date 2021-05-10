e04492cd13f363e7bdd4c0adaeb74fa9bdf10577
Josiah <jwalutsachi4@gmail.com>
2021-05-10 21:12:26 +0300

    Added new models to ./app/models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     questions_list = db.relationship('Questions', backref='questions')
+    examiner = db.Column(db.Integer, db.ForeignKey('examiners.id'))
 
 class Questions(db.Model):
     __tablename__ = 'questions'
@@ -15,6 +16,26 @@ class Questions(db.Model):
     option_c = db.Column(db.String(128), index=True)
     option_d = db.Column(db.String(128), index=True)
     quiz_id = db.Column(db.Integer, db.ForeignKey('quizes.id'))
+
+class Examiner(db.Model):
+    __tablename__ = 'examiners'
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(64), index=True)
+    lname = db.Column(db.String(64), index=True)
+
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True) 
+    fname = db.Column(db.String(64), index=True)
+    lname = db.Column(db.String(64), index=True)
+
+class AttemptedQuizes(db.Model):
+    __tablename__ = 'attemptedquizes'
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizes.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+    student_score = db.Column(db.Integer)
+    
     
     
 app = create_app('default')
