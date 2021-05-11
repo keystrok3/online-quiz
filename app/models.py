66ce938d1e5011db99ecddf46ef6d6ae9921b8e8
Josiah <jwalutsachi4@gmail.com>
2021-05-11 11:28:36 +0300

    Added password functionality to User Models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import create_app, db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Quiz(db.Model):
     __tablename__ = 'quizes'
@@ -22,13 +23,37 @@ class Examiner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(64), index=True)
     lname = db.Column(db.String(64), index=True)
+    password_hash = db.Column(db.String(128))
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Student(db.Model):
     __tablename__ = 'students'
     id = db.Column(db.Integer, primary_key=True) 
     fname = db.Column(db.String(64), index=True)
     lname = db.Column(db.String(64), index=True)
+    password_hash = db.Column(db.String(128))
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
 class AttemptedQuizes(db.Model):
     __tablename__ = 'attemptedquizes'
     id = db.Column(db.Integer, primary_key=True)
