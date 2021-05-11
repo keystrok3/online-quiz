2265013e3ed4a8704e90f9aa009aff52fbfcd02b
Josiah <jwalutsachi4@gmail.com>
2021-05-11 15:34:24 +0300

    Test functionality and db

--- manage.py
+++ manage.py
@@ -12,6 +12,13 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
+@manager.command
+def test():
+    """ Run The unit tests """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
     manager.run()
