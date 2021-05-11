614391a45baac58636b1f70c40641806907e8a04
Josiah <jwalutsachi4@gmail.com>
2021-05-11 21:09:10 +0300

    Updated view function for /register route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,29 +1,20 @@
 from . import auth
 from flask import request
-from ..models import Student, Examiner
+from ..models import User
 from app.models import db
 
 
-@auth.route('/registerstudent', methods=['GET', 'POST'])
-def registerstudent():
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
-        student = request.get_json()
-        newstudent = Student()
-        newstudent.fname = student['fname']
-        newstudent.lname = student['lname']
-        db.session.add(newstudent)
-        db.session.commit()
-        return 'Successful'
-    return 'Problem'
-
-@auth.route('/registerexaminer', methods=['GET', 'POST'])
-def registerexaminer():
-    if request.method == 'POST':
-        examiner = request.get_json()
-        newexaminer = Examiner()
-        newexaminer.fname = examiner['fname']
-        newexaminer.lname = examiner['lname']
-        db.session.add(newexaminer)
+        newuser = request.get_json()
+        user = User()
+        user.fname = newuser['fname']
+        user.lname = newuser['lname']
+        user.email = newuser['email']
+        user.password = newuser['password']
+        user.role = newuser['role']
+        db.session.add(user)
         db.session.commit()
         return 'Successful'
     return 'Problem'
